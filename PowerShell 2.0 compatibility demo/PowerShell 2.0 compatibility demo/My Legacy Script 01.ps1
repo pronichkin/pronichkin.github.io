5fb5f34b3b274a6dd14aaec02e77aaf59a835b78
Artem Pronichkin <artem@pronichkin.com>
2025-07-10 14:10:00 -0700

    removed script duplicates

--- PowerShell 2.0 compatibility demo/PowerShell 2.0 compatibility demo/My Legacy Script 01.ps1
+++
@@ -1,9 +0,0 @@
-$current  = Split-Path -Parent $MyInvocation.MyCommand.Path
-$path     = Join-Path -Path $current -ChildPath 'initialize.ps1'
-& $path
-$object   = New-Object -TypeName 'PowerShell20demo.CompatibilityDemo'
-
-Write-Verbose -Verbose -Message 'Test 1'
-Write-Verbose -Verbose -Message 'Try obtaining CAS policy'
-
-$object.GetCasPolicy()
