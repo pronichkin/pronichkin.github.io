04e5fdd8d471f8cd0c11aac89fe7706f7fdba1ed
Artem Pronichkin <artem@pronichkin.com>
2025-07-10 14:00:09 -0700

    added scripts to the solution

---
+++ PowerShell 2.0 compatibility demo/PowerShell 2.0 compatibility demo/My Legacy Script 03.ps1
@@ -0,0 +1,15 @@
+﻿$current  = Split-Path -Parent $MyInvocation.MyCommand.Path
+$path     = Join-Path -Path $current -ChildPath 'initialize.ps1'
+& $path
+$object   = New-Object -TypeName 'PowerShell20demo.CompatibilityDemo'
+
+Write-Verbose -Verbose -Message 'Test 3'
+Write-Verbose -Verbose -Message 'Format current date for the US'
+$object.GetDate( 'en-US' )
+
+Write-Verbose -Verbose -Message 'Format current date for France'
+$object.GetDate( 'fr-FR' )
+
+Write-Verbose -Verbose -Message 'Format current date for... oops, user made a typo'
+Write-Verbose -Verbose -Message 'This should be handled gracefully without crashing'
+$object.GetDate( 'xx-YY' )
